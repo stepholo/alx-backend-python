fb242d4afd73c83494e5e245a765741728a51ec6
stepholo <oloobrian89@gmail.com>
2024-02-11 12:47:59 +0000

    test utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,9 +16,31 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map: dict,
-                               path: tuple, expected_result: any): -> None:
+                               path: tuple, expected_result: any) -> None:
         """Method to test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map: dict,
+                               path: tuple, expected_result: any) -> None:
+        """Method to test if equal to """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
+        ])
+    def test_access_nested_map_exception(self, nested_map: dict, path: tuple,
+                                         expected_exception_msg: str) -> None:
+        """Method to test access_nested_map execption"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), expected_exception_msg)
 
 
 if __name__ == '__main__':
