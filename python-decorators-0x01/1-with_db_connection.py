05bf8985f35fa2444de7a88f243a9bcd5766b1db
stepholo <oloobrian89@gmail.com>
2025-05-20 17:29:00 +0300

    a decorator that automatically handles opening and closing database connections

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sqlite3
+import functools
+
+
+def with_db_connection(func):
+    """Implementing a decorator with_db_connection that opens
+       a database connection, passes it to the function and closes it afterword
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+        return result
+    return wrapper
+
+
+@with_db_connection
+def get_user_by_id(conn, user_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    return cursor.fetchone()
