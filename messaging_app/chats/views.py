eafff78f7d185843c673be9dcb47f543cda7741a
stepholo <oloobrian89@gmail.com>
2025-06-01 22:40:08 +0300

    Create views for serializers

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Conversation model.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Message model.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """Override to set the sender to the current user."""
+        serializer.save(sender=self.request.user)
