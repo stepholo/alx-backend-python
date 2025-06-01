df18ca27559229a999209d4e974936cd3529a4aa
stepholo <oloobrian89@gmail.com>
2025-06-01 22:49:12 +0300

    Create views for serializers

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
@@ -11,6 +13,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -20,7 +24,14 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['content']
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer: MessageSerializer) -> None:
         """Override to set the sender to the current user."""
         serializer.save(sender=self.request.user)
+
+    @action(detail=False, methods=['get'])
+    def custom_status(self, request: None) -> Response:
+        """Example custom action using status."""
+        return Response({'detail': 'Custom status OK'}, status=status.HTTP_200_OK)
