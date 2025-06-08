f380f5c7ea5473016d8979f602bdfa05ed249d62
stepholo <oloobrian89@gmail.com>
2025-06-08 18:35:24 +0300

    Adding pagination

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -9,7 +9,7 @@ class StandardResultsSetPagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response({
-            'count': self.page.pagination.count,
+            'count': self.page.paginator.count,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
             'results': data
