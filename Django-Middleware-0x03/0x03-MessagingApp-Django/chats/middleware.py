4c65a83e9f1450b7ef74c8f067e67ded37fb8f92
stepholo <oloobrian89@gmail.com>
2025-06-08 23:36:20 +0300

    File configuration

--- Django-Middleware-0x03/0x03-MessagingApp-Django/chats/middleware.py
+++ Django-Middleware-0x03/0x03-MessagingApp-Django/chats/middleware.py
@@ -66,7 +66,7 @@ class OffensiveLanguageMiddleware:
         return ip
 
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
