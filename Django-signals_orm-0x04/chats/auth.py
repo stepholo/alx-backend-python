6fc00c8e0f5316f1b97115cfd842755be68e7cc7
stepholo <oloobrian89@gmail.com>
2025-06-11 22:37:54 +0300

    Django signals

--- Django-signals_orm-0x04/chats/auth.py
+++
@@ -1,6 +0,0 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-
-class CustomJWTAuthentication(JWTAuthentication):
-    """Custom JWT authentication for future user"""
-    pass
